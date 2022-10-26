7d2353e5ad7c4d0e9c0e22637b403823887489ed
GitExcited <josedruiz1@gmail.com>
2022-10-26 18:11:01 -0400

    Driver 2

--- src/First/FlyingObject.java
+++ src/First/FlyingObject.java
@@ -1,5 +1,15 @@
 package First;
 
-public abstract class FlyingObject {
+public class FlyingObject {
     protected double price;
+    public FlyingObject(){
+        price = 1000;
+    }
+    public FlyingObject( FlyingObject fo){
+        price = fo.price;
+    }
+
+    public String toString(){
+        return "This is a flying object and it costs"+price;
+    }
 }
