205ecb0c9c4083b3d2b755e2b12e9d19b50d3e4b
GitExcited <josedruiz1@gmail.com>
2022-10-31 12:10:55 -0400

    hello3

--- src/Fifth/AgriculturalDrone.java
+++ src/Fifth/AgriculturalDrone.java
@@ -2,23 +2,38 @@ package Fifth;
 
 import Fourth.UAV;
 
+//Agricultural drone class
 public class AgriculturalDrone extends UAV {
+    //Brand attribute
     private String brand;
+    //carry capacity attribute
     private int carryCapacity;
 
     // Constructors
+    /**
+     * Default constructor sets carry capacity to 50 and brand to Netflix
+     */
     public AgriculturalDrone() {
         super();
         brand = "Netflix";
         carryCapacity = 50;
     }
-
+    /**
+     * Parametrized constructor
+     * @param _weight A.Drone weight
+     * @param _price A.Drone price
+     * @param _brand A.Drone brand
+     * @param _cap A.Drone carry capacity
+     */
     public AgriculturalDrone(double _weight, double _price, String _brand, int _cap) {
         super(_weight, _price);
         brand = _brand;
         carryCapacity = _cap;
     }
-
+    /**
+     * Copy constructor 
+     * @param a Object to be copied into new A. Drone
+     */
     public AgriculturalDrone(AgriculturalDrone a) {
         super(a.weight, a.price);
         brand = a.brand;
@@ -26,29 +41,51 @@ public class AgriculturalDrone extends UAV {
     }
 
     // Accessors
+    /**
+     * Access this object's brand
+     * @return A. Drone brand
+     */
     public String getBrand() {
         return this.brand;
     }
-
+    /**
+     * Access this object's carry capacity
+     * @return A.Drone carry capacity
+     */
     public int getCarryCapacity() {
         return this.carryCapacity;
     }
 
     // Mutators
-    public void setBrand(String brand) {
-        this.brand = brand;
+    /**
+     * Modifies this object's brand
+     * @param brand new brand to be changed
+     */
+    public void setBrand(String _brand) {
+        brand = _brand;
     }
-
-    public void setCarryCapacity(int carryCapacity) {
-        this.carryCapacity = carryCapacity;
+    /**
+     * Modifies carry capacity 
+     * @param _carryCapacity new carry capacity
+     */
+    public void setCarryCapacity(int _carryCapacity) {
+        carryCapacity = _carryCapacity;
     }
 
     // toString
+    /**
+     * to String method
+     * @return String containing all the information about this A. Drone including its attributes
+     */
     public String toString() {
         return super.toString() + " Its brand is " + brand + " and its carry capacity is " + carryCapacity + ".";
     }
 
     // equals
+    /** 
+     * Equals method
+     * @return true if object is an A.Drone and has the same attributes and false for any other case.
+     */
     public boolean equals(Object x) {
         if (x == null || this.getClass() != x.getClass())
             return false;
--- src/First/FlyingObject.java
+++ src/First/FlyingObject.java
@@ -14,12 +14,14 @@ public class FlyingObject {
         price = 1000;
     }
     /**
-     * Default constructor sets price to 1000;
+     * Copy constructor;
+     * @param fo object that will be copied in the constructor
      */
     public FlyingObject( FlyingObject fo){
         price = fo.price;
     }
     /**ToString method prints the attributes of the flying object
+     * @return returns string with the attributes of the flying object
      */
     public String toString(){
         return "This is a flying object and it costs"+price;
